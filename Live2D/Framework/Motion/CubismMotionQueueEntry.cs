8fac390543e0885774f2ae72e6823728548bfe31
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2026-02-24 20:09:37 +0530

    first

---
+++ Live2D/Framework/Motion/CubismMotionQueueEntry.cs
@@ -0,0 +1,102 @@
+﻿namespace PersonaEngine.Lib.Live2D.Framework.Motion;
+
+public class CubismMotionQueueEntry
+{
+    public CubismMotionQueueEntry()
+    {
+        Available = true;
+        StartTime = -1.0f;
+        EndTime   = -1.0f;
+    }
+
+    /// <summary>
+    ///     Motion
+    /// </summary>
+    public required ACubismMotion Motion { get; set; }
+
+    /// <summary>
+    ///     Activation flag
+    /// </summary>
+    public bool Available { get; set; }
+
+    /// <summary>
+    ///     Completion flag
+    /// </summary>
+    public bool Finished { get; set; }
+
+    /// <summary>
+    ///     Start flag (since 0.9.00)
+    /// </summary>
+    public bool Started { get; set; }
+
+    /// <summary>
+    ///     Motion playback start time [seconds]
+    /// </summary>
+    public float StartTime { get; set; }
+
+    /// <summary>
+    ///     Fade-in start time (only the first time for loops) [seconds]
+    /// </summary>
+    public float FadeInStartTime { get; set; }
+
+    /// <summary>
+    ///     Scheduled end time [seconds]
+    /// </summary>
+    public float EndTime { get; set; }
+
+    /// <summary>
+    ///     Time state [seconds]
+    /// </summary>
+    public float StateTime { get; private set; }
+
+    /// <summary>
+    ///     Weight state
+    /// </summary>
+    public float StateWeight { get; private set; }
+
+    /// <summary>
+    ///     Last time checked by the Motion side
+    /// </summary>
+    public float LastEventCheckSeconds { get; set; }
+
+    public float FadeOutSeconds { get; private set; }
+
+    public bool IsTriggeredFadeOut { get; private set; }
+
+    /// <summary>
+    ///     Set the start of fade-out.
+    /// </summary>
+    /// <param name="fadeOutSeconds">Time required for fade-out [seconds]</param>
+    public void SetFadeout(float fadeOutSeconds)
+    {
+        FadeOutSeconds     = fadeOutSeconds;
+        IsTriggeredFadeOut = true;
+    }
+
+    /// <summary>
+    ///     Start fade-out.
+    /// </summary>
+    /// <param name="fadeOutSeconds">Time required for fade-out [seconds]</param>
+    /// <param name="userTimeSeconds">Accumulated delta time [seconds]</param>
+    public void StartFadeout(float fadeOutSeconds, float userTimeSeconds)
+    {
+        var newEndTimeSeconds = userTimeSeconds + fadeOutSeconds;
+        IsTriggeredFadeOut = true;
+
+        if ( EndTime < 0.0f || newEndTimeSeconds < EndTime )
+        {
+            EndTime = newEndTimeSeconds;
+        }
+    }
+
+    /// <summary>
+    ///     Set the motion state.
+    /// </summary>
+    /// <param name="timeSeconds">Current time [seconds]</param>
+    /// <param name="weight">Motion weight</param>
+    public void SetState(float timeSeconds, float weight)
+    {
+        StateTime   = timeSeconds;
+        StateWeight = weight;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+
+using PersonaEngine.Lib.Live2D.App;
+using PersonaEngine.Lib.Live2D.Framework.Rendering;
+using PersonaEngine.Lib.Live2D;
+
+class Program
+{
+	static IWindow window; // silk.NET window
+	static GL gl; // GL api ecxtracted from window
+
+	static LAppDelegate? lapp; // live2d app that draws to/through GL
+
+	static void Main()
+	{
+		WindowOptions options = WindowOptions.Default;
+		options.Size = new() { X = 800, Y = 400 };
+		options.Title = "Hello";
+
+		window = Window.Create(options);
+		window.Load += OnLoad;
+		window.Render += OnRender; // frame renderer
+		window.Run();
+	}
+
+	static void OnLoad()
+	{
+		gl = GL.GetApi(window);
+		lapp = new(new SilkNetApi(gl, 800, 400), _ => { });
+		lapp.Live2dManager.LoadModel(
+			@"C:\Users\Jayakuttan\dev\csharp-live2d\models\Luna", "Luna"
+		);
+	}
+
+	static void OnRender(double delta)
+	{
+		if (lapp == null) return;
+		lapp.Update((float)delta);
+		lapp.Run();
+	}
+}
